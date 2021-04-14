b43ad90563842e77071812a70e12c62b661723ab
printSamarth <itssp1998@gmail.com>
2021-04-14 19:10:46 +0530

    Added python backend for stocks

---
+++ Stocks python/stocks_fetcher.py
@@ -0,0 +1,40 @@
+import sys
+import requests
+import json
+import time
+from datetime import date
+from datetime import datetime
+from nsetools import Nse
+from flask import Flask, request, jsonify
+from firebase import firebase  
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+cors = CORS(app)
+
+@app.route('/api/<code>',methods=['GET'])
+@cross_origin()
+def price(code):
+	#print("got request")
+	#print(code)
+	nse = Nse()
+	stock_code = nse.get_stock_codes()
+	#stocks = ['20MICRONS', '3IINFOTECH','3MINDIA','8KMILES','A2ZINFRA','AARTIDRUGS','AARTIIND']
+	dic = {}
+	#for code in stocks:
+	out = nse.get_quote(code)
+	dic[code] = out['lastPrice']
+	print(dic)
+	with open("sample.json", "w") as outfile: 
+	    json.dump(dic, outfile)
+	with open('sample.json') as data_file:
+		data = json.load(data_file)
+	#print(data)
+	return data
+
+if __name__ == '__main__':
+    try:
+        app.run( port=5000, debug=True)
+    except:
+        print("Server is exited unexpectedly. Please contact server admin.")
+
